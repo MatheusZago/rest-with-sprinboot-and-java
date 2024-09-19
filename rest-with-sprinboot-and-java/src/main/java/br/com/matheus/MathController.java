cc9ed66ca4db5c1b7f0cdd26fce9dfbe0495b229
Matheus <matheuszago134@gmail.com>
2024-09-19 18:42:34 -0300

    Add other mathematical operations

--- rest-with-sprinboot-and-java/src/main/java/br/com/matheus/MathController.java
+++ rest-with-sprinboot-and-java/src/main/java/br/com/matheus/MathController.java
@@ -25,6 +25,49 @@ public class MathController {
 		return convetToDouble(numberOne) + convetToDouble(numberTwo);
 	}
 
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sub(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumerico(numberOne) || !isNumerico(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value.");
+		}
+
+		return convetToDouble(numberOne) - convetToDouble(numberTwo);
+	}
+
+	@RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double mult(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumerico(numberOne) || !isNumerico(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value.");
+		}
+
+		return convetToDouble(numberOne) * convetToDouble(numberTwo);
+	}
+
+	@RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double div(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumerico(numberOne) || !isNumerico(numberTwo)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value.");
+		}
+
+		return convetToDouble(numberOne) / convetToDouble(numberTwo);
+	}
+
+	@RequestMapping(value = "/root/{numberOne}", method = RequestMethod.GET)
+	public Double root(@PathVariable(value = "numberOne") String numberOne) throws Exception {
+
+		if (!isNumerico(numberOne)) {
+			throw new UnsupportedMathOperationException("Please set a numeric value.");
+		}
+
+		return Math.sqrt(convetToDouble(numberOne));
+	}
+
 	private Double convetToDouble(String strNumber) {
 		if (strNumber == null)
 			return 0D;
