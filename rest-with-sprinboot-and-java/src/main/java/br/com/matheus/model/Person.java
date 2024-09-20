933d0241f19e548e8282f382a82b4073667f7f83
Matheus <matheuszago134@gmail.com>
2024-09-20 13:39:49 -0300

    Add Database connection and create Person Repository

--- rest-with-sprinboot-and-java/src/main/java/br/com/matheus/model/Person.java
+++ rest-with-sprinboot-and-java/src/main/java/br/com/matheus/model/Person.java
@@ -3,32 +3,39 @@ package br.com.matheus.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // Para dizer que é uma entidade de banco de dados
+@Table(name = "person") // Para dizer qual tabela ele se refere
 public class Person implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	private long id;
+	@Id // Para falar q é a primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Fazendo o auto increment
+	private Long id;
+	@Column(name = "first_name", nullable = false, length = 80)
 	private String firstName;
+	@Column(name = "last_name", nullable = false, length = 80)
 	private String lastName;
+	@Column(name = "address", nullable = false, length = 100)
 	private String address;
+	@Column(name = "gender", nullable = false, length = 6)
 	private String gender;
 
 	public Person() {
 
 	}
 
-//	public Person(long id, String firstName, String lastName, String address, String gender) {
-//		this.id = id;
-//		this.firstName = firstName;
-//		this.lastName = lastName;
-//		this.address = address;
-//		this.gender = gender;
-//	}
-
 	public long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
