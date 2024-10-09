ab124d4bcd5a8033f68cc70ad76c22c4ea6dd574
Matheus <matheuszago134@gmail.com>
2024-10-09 15:09:43 -0300

    Add migration with flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (id, address, first_name, gender, last_name) VALUES
+(1,'São Paulo','Ayrton','Male','Senna'),
+(2,'Anchiano - Italy','Leonardo','Male','da Vinci'),
+(4,'Kentucky  - US','Muhammed','Male','Ali'),
+(5,'RJ  - Brazil','William','Male','Bonner'),
+(7,'Vezo  - Africa do Sul','Nelson','Male','Mandela'),
+(8,'Vezo  - Africa do Sul','Nelson','Male','Mandela'),
+(9,'Smijan  - Croacia','Nikola','Male','Tesla');
