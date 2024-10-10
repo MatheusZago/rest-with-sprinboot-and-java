2f6a025186592ace93f689f427c7dc22a3c7fecb
Matheus <matheuszago134@gmail.com>
2024-10-10 17:05:44 -0300

    Add cors globally

--- src/main/java/br/com/matheus/config/WebConfig.java
+++ src/main/java/br/com/matheus/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.matheus.config;
 
 import br.com.matheus.serialization.converter.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +16,23 @@ public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**") //ta mapeando todas as rotas da api para iss
+//                .allowedMethods("GET", "POST", "PUT")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true); //Para liberar authenticação
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
@@ -38,8 +57,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
     }
 
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.add(new YamlJackson2HttpMessageConverter());
-    }
 }
