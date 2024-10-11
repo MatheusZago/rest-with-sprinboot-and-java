022a0e5278c38c4baed8c7402cb28aa1f3f95ad2
Matheus <matheuszago134@gmail.com>
2024-10-11 13:20:53 -0300

    Add SecurityConfig with e

--- src/main/java/br/com/matheus/security/jwt/JwtConfigurer.java
+++ src/main/java/br/com/matheus/security/jwt/JwtConfigurer.java
@@ -3,8 +3,10 @@ package br.com.matheus.security.jwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
