9cb10dce391e82f8738512bddd39c0163f18d17b
Matheus <matheuszago134@gmail.com>
2024-10-14 16:56:52 -0300

    Add ordenation by firstName

--- src/main/java/br/com/matheus/controllers/PersonController.java
+++ src/main/java/br/com/matheus/controllers/PersonController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -63,8 +64,11 @@ public class PersonController {
 					@ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content),
 			})
 	public ResponseEntity<Page<PersonVO>> findByAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-													@RequestParam(value = "limit", defaultValue = "12") Integer limit) throws Exception {
-		Pageable pageable = PageRequest.of(page, limit);
+													@RequestParam(value = "limit", defaultValue = "12") Integer size,
+													@RequestParam(value = "direction", defaultValue = "asc") String direction) throws Exception {
+
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Sort.Direction.DESC : Sort.Direction.ASC;
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
 
 		return ResponseEntity.ok(service.findAll(pageable));
 	}
