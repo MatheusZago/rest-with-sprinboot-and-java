104c5750e6af06f344a56ebbf7642378b3a2d0fb
Matheus <matheuszago134@gmail.com>
2024-10-15 17:37:40 -0300

    Add fileDownload

--- src/main/java/br/com/matheus/controllers/FileController.java
+++ src/main/java/br/com/matheus/controllers/FileController.java
@@ -4,12 +4,14 @@ import br.com.matheus.data.vo.v1.UploadFileResponseVO;
 import br.com.matheus.exceptions.MyFileNotFoundException;
 import br.com.matheus.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -23,6 +25,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/file/v1")
 public class FileController {
 
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(FileController.class);
     private Logger logger = Logger.getLogger(FileController.class.getName());
 
     @Autowired
@@ -49,6 +52,24 @@ public class FileController {
        return Arrays.asList(files).stream().map(file -> uploadFile(file)).collect(Collectors.toList());
     }
 
+    @GetMapping("/downloadFile/{filename:.+}") //Isso é para pegar as extensões "tipo .txt"
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename, HttpServletRequest request){
+        logger.info("Reading file on disk");
 
+        Resource resource = service.loadFileAsResource(filename);
+        String contentType = "";
+        try{
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        }catch (Exception e){
+            logger.info("Could not determine file type.");
+        }
+
+        if(contentType.isBlank()) contentType = "application/octet-strea";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
 
 }
